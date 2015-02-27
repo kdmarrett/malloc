dfb1ed56c4ce6195e7f6d59e3f43bd89639c5349
NAN JIANG <nj8@u.washington.edu>
2015-02-26 23:52:03 -0800

    testing version of getmem.c

--- bench.c
+++ bench.c
@@ -12,8 +12,29 @@
 #include "mem.h"
 #include "mem_impl.h"
 
+extern newBlock * free_list;
 
 int main(int argc, char** argv) {
+	//manually add block
+	free_list = malloc(500);
+	free_list->size = 500-sizeof(*free_list);
+	newBlock* h1;
+	h1 = malloc(700);
+	h1->size = 700 - sizeof(*h1);
+	free_list->next = h1;
+	newBlock* h2;
+	h2 = malloc(5000);
+	h2->size = 5000 - sizeof(*h2);
+	h1->next = h2;
+
+	newBlock* h3;
+	h3 = malloc(6000);
+	h3->size = 6000 - sizeof(*h3);
+	h2->next = h3;
+	//testing getmem
+	getmem(3200); 
+	//getmem(200);
+	//getmem(500);
 	//testing print_heap
 	FILE *f;
 	f = fopen("test", "w+");
--- getmem.c
+++ getmem.c
@@ -12,9 +12,81 @@
 #include "mem.h"
 #include "mem_impl.h"
 
+#define MSIZE 4000
+
 newBlock* free_list = NULL;
 
 void* getmem(uintptr_t size) {	
-	void* p = NULL;
-	return p;
+	if(size < 0) {
+		return NULL;
+	}else if(free_list == NULL) {
+		newBlock* node;
+		if(size > MSIZE || size == MSIZE) {
+			node = malloc(size);
+			node->size= size;
+		}else {
+			newBlock* temp;
+			temp = malloc(MSIZE);
+			temp->size = MSIZE;
+			node = temp + sizeof(*temp) + temp->size - size; 
+			free_list = temp;
+			free_list->size = MSIZE - sizeof(*temp) - size;
+		}
+		return node;
+	}
+	newBlock* h1;
+	newBlock* h2;
+	h1 = free_list;
+	h2 = free_list;
+	if(h1->size > size) {
+		newBlock* tempN;
+		tempN = h1 + sizeof(*tempN) + h1->size - size; 
+		h1->size = h1->size - size;
+		tempN->size = size;
+		return tempN;
+	}else {
+		while(h1->next != NULL) {
+			if((h1->next)->size > size) {
+				printf("123\n");
+				h2 = h2->next;
+				if((h1->next)->next != NULL) {
+					h1->next = h2->next;
+					h2->next = NULL;
+				}else {
+					h1->next = NULL;
+				}
+				newBlock* temp;
+				temp = h2 + sizeof(*h2)+h2->size-size;
+				temp->size = size;
+				h2->size = h2->size-size;
+				int test = (int) h2->size;
+				printf("%d", test);
+				//put h2 back in free_list
+				h1 = free_list;
+				if(h2->size < h1->size) {
+					h2->next = h1;
+					free_list = h2;				
+				} else {
+					while(h1->next != NULL) {
+						if((h1->next)->size > h2->size) {
+							h2->next = h1->next;
+							h1 = h2;
+						}else {
+							h1 = h1->next;
+						}
+					}
+					h1->next = h2;
+					return temp;
+				}
+				
+			}
+				//printf("123\n");
+				h1 = h1->next;
+				h2 = h2->next;
+		}
+		newBlock* create;
+		create = malloc(size);
+		create->size= size;
+		return create;
+	}
 }

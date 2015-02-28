75a39728e29a6c2f0253ee7fa655eed4d073c229
NAN JIANG <nj8@u.washington.edu>
2015-02-27 19:56:57 -0800

    correct version of getmem.c

--- getmem.c
+++ getmem.c
@@ -12,6 +12,7 @@
 
 newBlock* free_list = NULL;
 uintptr_t tsize = 0;
+uintptr_t hsize = sizeof(newBlock*) + sizeof(uintptr_t);
 
 void* getmem(uintptr_t size) {	
 	if(size < 0) { //if size is less than zero
@@ -21,21 +22,21 @@ void* getmem(uintptr_t size) {
 		newBlock* node;
 		if(size > MSIZE || size == MSIZE) { //if it's too big, create a block that satisfy the needs
 			tsize = tsize + size;		
-			node = malloc(size);
+			node = malloc(size + hsize);
 			node->size= size;
 		}else if(size < MSIZE && MSIZE-size < THRESHOLD){ //within threshold, return the whole block
 			tsize = tsize + MSIZE;
-			node = malloc(MSIZE);
+			node = malloc(MSIZE + hsize);
 			node->size= MSIZE;
 		}else { //return the needed memory, and put the rest back to free_list
 			tsize = tsize + MSIZE;			
 			newBlock* temp;
-			temp = malloc(MSIZE);
+			temp = malloc(MSIZE + hsize);
 			temp->size = MSIZE;
-			node = temp + sizeof(*temp) + temp->size - size; //check this later (address might be wrong)
+			node = temp + temp->size - size; //node=temp+hsize+temp->size-soze-hsize
 			node->size = size; 
 			free_list = temp;
-			free_list->size = MSIZE - sizeof(*temp) - size;
+			free_list->size = MSIZE - size -hsize; //minus the size of next and uint
 		}
 		return node;
 	}
@@ -48,29 +49,29 @@ void* getmem(uintptr_t size) {
 	if(h1->size > size) { //the first one is big enough 
 		newBlock* tempN;
 		if((h1->size-size) > THRESHOLD) { //return the needed memory, and put the rest back to free_list
-			tempN = h1 + sizeof(*tempN) + h1->size - size; 
-			h1->size = h1->size - size;
+			tempN = h1 + h1->size - size; //same as temp above
+			h1->size = h1->size - size - hsize; //minus the size of next and uint
 			tempN->size = size;
 		} else {//within the threshold, return the whole block
-			tempN = h1+sizeof(*tempN);
-			free_list = h1->next;
+			tempN = h1;
+			free_list = h1->next; 
 			tempN->size = h1->size;
 		}
 		return tempN;
 	}else if(h1->size == size) { //if needed size is equal to the size of first block, return the first block
 		free_list = h2->next;
-		h1 = h1 + sizeof(*h1);
+		h1 = h1;
 		h1->size = size;
 		return h1;
-	}else { //if the size of first block is smaller than needed
+	}else { //if the size of first block is smaller than needed, go through the list
 		while(h1->next != NULL) {
 			if((h1->next)->size > size || (h1->next)->size == size) { 
 				h2 = h2->next;
-				if(h1->next->size == size || (h1->next->size-size) < THRESHOLD) {
+				if(h1->next->size == size || (h1->next->size-size) < THRESHOLD) { //return the whole block
 					uintptr_t tempSize = h1->next->size;
 					h1->next = h2->next;
 					h2->next = NULL;
-					h2 = h2 +sizeof(*h2);
+					//h2 = h2;
 					h2->size = tempSize;
 					return h2;
 				}else if(h2->next != NULL) {
@@ -80,9 +81,9 @@ void* getmem(uintptr_t size) {
 					h1->next = NULL;
 				}
 				newBlock* temp;
-				temp = h2 + sizeof(*h2)+h2->size-size;
+				temp = h2 + h2->size-size; //temp = h2+hsize+h2->size-size-hsize
 				temp->size = size;
-				h2->size = h2->size-size;
+				h2->size = h2->size-size-hsize; //minus the size of next and uint
 				//put h2 back in free_list
 				h1 = free_list;
 				if(h2->size < h1->size) {
@@ -109,7 +110,7 @@ void* getmem(uintptr_t size) {
 		}
 		tsize = tsize + size;
 		newBlock* create;
-		create = malloc(size);
+		create = malloc(size + hsize); //malloc extra space for the struct
 		create->size= size;
 		return create;
 	}
